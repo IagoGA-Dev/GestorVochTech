9e632fabd59bcace2d86c9ce51c357593606b351
Iago Araújo <iagogaraujo@gmail.com>
2025-02-12 13:25:51 -0300

    Adicionando tabelas Bandeira, Colaborador, GrupoEconomico e Unidade

---
+++ app/Models/Bandeira.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bandeira extends Model {
+    use HasFactory;
+
+    protected $table = 'bandeiras';
+
+    protected $fillable = [
+        'nome',
+        'grupo_economico_id',
+    ];
+
+    public function grupoEconomico() {
+        return $this->belongsTo(GrupoEconomico::class);
+    }
+
+    public function unidades() {
+        return $this->hasMany(Unidade::class);
+    }
+}
+
---
+++ app/Models/GrupoEconomico.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GrupoEconomico extends Model
+{
+    use HasFactory;
+
+    protected $table = 'GrupoEconomico';
+
+    protected $fillable = [
+        'nome',
+    ];
+
+    public function bandeiras() {
+        return $this->hasMany(Bandeira::class);
+    }
+}
